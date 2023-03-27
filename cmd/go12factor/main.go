1b59914342d47a4f17bef8fd0a82519c34dfa646
Skif <breakthevoid@gmail.com>
2023-03-28 06:04:43 +0700

    Factor 9. Graceful shutdown

--- cmd/go12factor/main.go
+++ cmd/go12factor/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -14,13 +20,29 @@ func main() {
 		logrus.Fatal("Port is not set")
 	}
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
 	})
 
-	err := http.ListenAndServe(":"+port, nil)
-
-	if err != nil {
-		return
+	serv := http.Server{
+		Addr:    net.JoinHostPort("", port),
+		Handler: router,
 	}
+
+	go func() {
+		err := serv.ListenAndServe()
+		if err != nil {
+
+		}
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	serv.Shutdown(timeout)
 }
