70e749307a2b59976511b42514d153e5d6c87ae5
Skif <breakthevoid@gmail.com>
2023-03-28 02:46:54 +0700

    Factor 3. Env configuration

--- cmd/go12factor/main.go
+++ cmd/go12factor/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
 
 func main() {
 	logrus.Info("Hello World!")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logrus.Fatal("Port is not set")
+	}
 }
